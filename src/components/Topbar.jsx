cc03cc9a0211647b35420ba4eb2f4496a2c6aeb6
Kunah <74835793+Kunah@users.noreply.github.com>
2021-12-12 19:02:37 +0100

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -12,7 +12,7 @@ export default class Topbar extends React.Component {
     render (){
         return (
             <div className={'text-white font-semibold fixed top-0 bg-gradient-to-r from-red-400 via-yellow-200 to-green-400 px-4 py-2 md:py-4 w-full flex items-center shadow'}>
-                <Link className={`flex items-center px-2 ${this.props.active == "home" ? "underline" : ""}`} to="/"><img className={'-my-4 pr-2'} width={'40px'} src={logo}/>React Electron app</Link>
+                <Link className={`flex items-center px-2 ${this.props.active == "home" ? "underline" : ""}`} to="/"><img className={'-my-4 pr-2'} width={'40px'} src={logo}/>React app</Link>
                 <div className={'flex-grow'}></div>
                 <a className={`px-2 ${this.props.active == "github" ? "underline" : ""}`} target="_blank" rel="noreferrer" href="https://github.com/Kunah/react-app-bp">GitHub Repo</a>
             </div>
